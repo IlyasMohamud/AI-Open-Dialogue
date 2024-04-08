9386e677f0b98abc12e9717b1db5d575c95ffdb6
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-08 16:41:50 +0200

    fixed bug missing parenthesis

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-3xl lg:text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2>
+          <h3 className="text-3xl lg:text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
           🔍 Master's student in Informatics | 🧠 Diving deep into machine learning & web dev
           🚀 Exploring tech, one project at a time | 🌱 Growing through code
           📚 Sharing my learning journey | 🤝 Let's connect!
@@ -14,22 +14,17 @@ export function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="x-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Github
             </a>
             <a
-              href={`https://github.com/IlyasMohamud`}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href={`https://www.linkedin.com/in/ilyasahmedmohamud/`}
+              className="x-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              
             >
-              Instagram
+              LinkedIn
             </a>
             
             
