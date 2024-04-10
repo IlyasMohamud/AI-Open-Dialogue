74f85467d97df89dab958e5a34dd38e27b3f0c78
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-10 13:54:46 +0200

    forgot to import CMS NAME

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,5 +1,6 @@
 import Container from "@/app/_components/container";
 import { EXAMPLE_PATH } from "@/lib/constants";
+import { CMS_NAME } from "@/lib/constants";
 
 export function Footer() {
   return (
