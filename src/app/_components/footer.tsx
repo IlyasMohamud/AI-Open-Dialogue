104fe99a506defb8f28a1b848d3ff36cf3917f54
Ilyas <ilyasmo_1999@hotmail.co.uk>
2024-04-10 13:29:33 +0200

    added instagram link to footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -28,6 +28,13 @@ export function Footer() {
             >
               LinkedIn
             </a>
+            <a
+              href={`https://www.instagram.com/sexysomalian99/`}
+              className="x-3 bg-pink-500 hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              
+            >
+              Instagram
+            </a>
             
             
           </div>
